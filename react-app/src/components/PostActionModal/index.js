a3b4bd3098d176236d87a0fc02bd997213d1bf48
Hui Feng <huifeng248@gmail.com>
2022-09-02 16:21:56 -0700

    modal issue

--- react-app/src/components/PostActionModal/index.js
+++ react-app/src/components/PostActionModal/index.js
@@ -3,11 +3,11 @@ import './PostActionModal.css'
 import { NavLink } from 'react-router-dom';
 import PostModal from '../PostModal'
 import { useState } from 'react';
-import {DeletePost} from '../../store/post'
+import { DeletePost } from '../../store/post'
 import { useDispatch, useSelector } from "react-redux";
 
 
-function PostActionModal({user, post, ShowPostActionModal, setShowPostActionModal}) {
+function PostActionModal({ user, post, ShowPostActionModal, setShowPostActionModal }) {
     const [showPostModal, setShowPostModal] = useState(false)
     const dispatch = useDispatch()
 
@@ -18,38 +18,38 @@ function PostActionModal({user, post, ShowPostActionModal, setShowPostActionModa
     //     }
     // }
     // const deletePostOnclick = async (post.id) => {
-	// 	if (
-	// 		window.confirm(
-	// 			"Do you really want to delete this Image? This action can not be undone!"
-	// 		)
-	// 	) {
-	// 		const response = await dispatch(DeleteImage(imageId));
-	// 		if (response) {
-	// 			window.alert(
-	// 				"Successfully deleted the Image, Click OK to bring you back to the home page"
-	// 			);
-	// 			history.push(`/`);
-	// 		}
-	// 	}
-	// };
-    
+    // 	if (
+    // 		window.confirm(
+    // 			"Do you really want to delete this Image? This action can not be undone!"
+    // 		)
+    // 	) {
+    // 		const response = await dispatch(DeleteImage(imageId));
+    // 		if (response) {
+    // 			window.alert(
+    // 				"Successfully deleted the Image, Click OK to bring you back to the home page"
+    // 			);
+    // 			history.push(`/`);
+    // 		}
+    // 	}
+    // };
+
 
     return (
         <div>
-            {ShowPostActionModal && 
-            <Modal onClose={()=>setShowPostActionModal(false)}>
-                <div className='post_action_button'>
-                    <button onClick={()=> {
-                        setShowPostActionModal(false)
-                        setShowPostModal(true)
+            {ShowPostActionModal &&
+                <Modal onClose={() => setShowPostActionModal(false)}>
+                    <div className='post_action_button'>
+                        <button onClick={() => {
+                            setShowPostActionModal(false)
+                            setShowPostModal(true)
                         }}>
-                            Edit Post</button> 
-                    <PostModal user={user} post={post} setShowPostModal={setShowPostModal} showPostModal={showPostModal} />
-                    <button onClick={()=> deletePostOnclick()}
-                    >Delete Post</button>
-                    <button onClick={()=> setShowPostActionModal(false)}>Cancel</button>
-                </div>
-            </Modal>}
+                            Edit Post</button>
+                        <PostModal user={user} post={post} setShowPostModal={setShowPostModal} showPostModal={showPostModal} />
+                        <button 
+                        >Delete Post</button>
+                        <button onClick={() => setShowPostActionModal(false)}>Cancel</button>
+                    </div>
+                </Modal>}
         </div>
     )
 }
