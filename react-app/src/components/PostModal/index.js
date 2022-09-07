672a28e21c7425a01958829ecdc371b86f161b93
Hui Feng <huifeng248@gmail.com>
2022-09-07 11:01:21 -0700

    url update2

--- react-app/src/components/PostModal/index.js
+++ react-app/src/components/PostModal/index.js
@@ -42,12 +42,12 @@ function PostModal({ user, post, setShowPostModal, showPostModal }) {
         if (!isValid) {
             errors_arr.push('Please provide a valid image url that ends with jpg, jpeg, png, webp, avif, gif, or svg')
         }
-        if (url.includes(' ')) {
+        if (url && url.includes(' ')) {
 			errors_arr.push(
 				'Cannot have an empty space in the url!'
 			)
 		}
-        if (url.includes("File:")) {
+        if (url && url.includes("File:")) {
 			errors_arr.push(
 				'Invalid URL: URL must not include "File:", Please use original image address'
 			);
