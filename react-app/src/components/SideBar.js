75920a2b8ef1614899699e149460271a745bcab2
Hui Feng <huifeng248@gmail.com>
2022-09-08 10:44:19 -0700

    sidebard css

--- react-app/src/components/SideBar.js
+++ react-app/src/components/SideBar.js
@@ -1,9 +1,23 @@
 import "./SideBar.css"
-
-function SideBar() {
+import { NavLink, Link } from 'react-router-dom';
+function SideBar({ user }) {
     return (
 
         <div className="sidebar_container">
+            <NavLink className="contact_info_link" to='/' exact={true} activeClassName='active'>
+                <div className="contact_info_link_div">
+                    <i class="fa-solid fa-house"></i>
+                    <div>Home</div>
+                </div>
+            </NavLink>
+            <NavLink className="contact_info_link" to={`/posts/users/${user.id}`} activeClassName='active'>
+                <div className="contact_info_link_div">
+                    <img className="user_profile_image_sidebar" src={user.profile_img}></img>
+                    <div>{user.first_name} {user.last_name}</div>
+                </div>
+            </NavLink>
+
+
             <a className="contact_info_link" href="https://www.linkedin.com/in/hui-abby-feng-cpa/">
                 <div className="contact_info_link_div">
                     <i className="fa-brands fa-linkedin"></i>
