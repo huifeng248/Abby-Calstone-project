a51dd8272cfa345c30871e6458761929a50015f3
Hui Feng <huifeng248@gmail.com>
2022-09-07 23:06:04 -0700

    adding links

--- react-app/src/components/PicCard/index.js
+++ react-app/src/components/PicCard/index.js
@@ -4,6 +4,8 @@ import { Load_Posts_Homepage } from '../../store/post'
 import PostCreateBox from './PostCreateBox'
 import CardDetail from './CardDetail'
 import './PicCard.css'
+import SideBar from "../SideBar";
+
 
 function PicCard() {
     const dispatch = useDispatch()
@@ -23,20 +25,24 @@ function PicCard() {
 
     // return "this is homepage"
     return (
-        <div className="outer_container">
-            <PostCreateBox user={user}/>
-            {/* <div className="create_post_container">
+        <div className="main_page_container">
+            <SideBar />
+
+            <div className="outer_container">
+                <PostCreateBox user={user} />
+                {/* <div className="create_post_container">
                 <div>
                     <img className="user_profile_image" src={user.profile_img}></img>
                 </div>
                 <input className="post_input"
                     placeholder={`What's on your mind, ${user.first_name}?`}></input>
             </div> */}
-            {loaded && post_arr.map((post, index) => {
-                return <div key={index} className="post_card_container">
-                    <CardDetail user={user} post={post}/>
-                </div>
-            })}
+                {loaded && post_arr.map((post, index) => {
+                    return <div key={index} className="post_card_container">
+                        <CardDetail user={user} post={post} />
+                    </div>
+                })}
+            </div>
         </div>
     )
 
---
+++ react-app/src/components/SideBar.js
@@ -0,0 +1,30 @@
+function SideBar() {
+    return (
+
+        <div className="sidebar_container">
+            <a className="contact_info_link" href="https://www.linkedin.com/in/hui-abby-feng-cpa/">
+                <div className="contact_info_link_div">
+                    <i className="fa-brands fa-linkedin"></i>
+                    <div>Abby Feng linkedin</div>
+                </div>
+            </a>
+
+            <a className="contact_info_link" href="https://github.com/huifeng248">
+                <div className="contact_info_link_div">
+                    <i class="fa-brands fa-github"></i>
+                    <div>Github</div>
+                </div>
+            </a>
+
+            <a className="contact_info_link" href="https://github.com/huifeng248/Abby-Calstone-project">
+                <div className="contact_info_link_div">
+                    <i class="fa-brands fa-github"></i>
+                    <div>About FaceTa</div>
+                </div>
+            </a>
+
+        </div>
+    )
+}
+
+export default SideBar
