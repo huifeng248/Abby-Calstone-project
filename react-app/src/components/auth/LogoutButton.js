99cb4d71e26be40036fd10cee115a5b88bcfd17e
Hui Feng <huifeng248@gmail.com>
2022-09-08 17:09:29 -0700

    css styling

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
+import "./LoginSignupForm.css"
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
@@ -8,7 +9,7 @@ const LogoutButton = () => {
     await dispatch(logout());
   };
 
-  return <button onClick={onLogout}>Logout</button>;
+  return <button className="log_out_button" onClick={onLogout}>Logout</button>;
 };
 
 export default LogoutButton;
